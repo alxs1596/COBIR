286cc2d4930caee9349e416269f00a7683417cda
Alexis <alxs1596@hotmail.com>
2017-05-12 20:54:57 -0500

    Clases de pancho implementadas y contador de tiempo incluido

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,58 @@
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import com.ulasalle.cobir.Buscador;
+import com.ulasalle.cobir.Caracteristica;
+import com.ulasalle.cobir.IndexadorParalelo;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		//IndexadorParalelo indexadorParalelo=new IndexadorParalelo();
+        //indexadorParalelo.setRutaBD("C:\\Users\\alxs1\\Desktop\\Pruebas y Calidad de Software\\lucene-datos-cobir\\nombre_mi_bd");
+        //indexadorParalelo.indexar("C:\\Users\\alxs1\\Desktop\\Pruebas y Calidad de Software\\coil-100\\coil-100",new Caracteristica.Opcion[]{Caracteristica.Opcion.AutoColorCorrelogram});
+
+        Buscador buscador=new Buscador();
 		
+        File imagen=new File("C:\\Users\\alxs1\\Desktop\\Pruebas y Calidad de Software\\coil-100\\coil-100\\obj57__0.png");
+        
+        long start = System.currentTimeMillis();
+        String[] resultado = buscador
+                .buscar(
+                        imagen,
+                        "C:\\Users\\alxs1\\Desktop\\Pruebas y Calidad de Software\\lucene-datos-cobir\\nombre_mi_bd",
+                        10,
+                        Caracteristica.getInstancia().getCaracteristica(Caracteristica.Opcion.AutoColorCorrelogram));
+        long tiempo  =(System.currentTimeMillis() - start);
+        System.out.println("Resultados obtenidos en " + buscador.tiempoTranscurrido + " milisegundos");
+        
+        JPanel jPanel = new JPanel(new GridLayout(1, resultado.length));
+        Arrays.stream(resultado)
+                .forEach(elemento->
+                {
+                	//System.out.println(elemento);
+                	
+                	BufferedImage imgRes =null;
+                	try{
+                		imgRes= ImageIO.read(new File(elemento));
+                	}catch(Exception e){}
+                    ImageIcon icon = new ImageIcon(imgRes);
+                    Image image = icon.getImage(); // transform it 
+                    Image newimg = image.getScaledInstance(120, 120,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+                    icon = new ImageIcon(newimg);
+                    JLabel label = new JLabel(icon);
+                    jPanel.add(label);
+                });
+        JOptionPane.showMessageDialog(null, jPanel);
 	}
 
 }
---
+++ src/main/java/com/ulasalle/cobir/Buscador.java
@@ -0,0 +1,42 @@
+package com.ulasalle.cobir;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import javax.imageio.ImageIO;
+
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.store.FSDirectory;
+
+import net.semanticmetadata.lire.builders.DocumentBuilder;
+import net.semanticmetadata.lire.imageanalysis.features.GlobalFeature;
+import net.semanticmetadata.lire.searchers.GenericFastImageSearcher;
+import net.semanticmetadata.lire.searchers.ImageSearchHits;
+import net.semanticmetadata.lire.searchers.ImageSearcher;
+
+public class Buscador
+{
+	public long tiempoTranscurrido = 0; 
+    public String[] buscar(File imagen, String rutaBD, int numeroResultados, Class<? extends GlobalFeature> caracteristica)
+    {
+        String[] imagenes=null;
+        try {
+            IndexReader lector= DirectoryReader.open(FSDirectory.open(Paths.get(rutaBD)));
+            ImageSearcher buscadorImagenes=new GenericFastImageSearcher(numeroResultados, caracteristica);
+            long start = System.currentTimeMillis();
+            ImageSearchHits resultados=buscadorImagenes.search(ImageIO.read(imagen),lector);
+            tiempoTranscurrido = System.currentTimeMillis()-start;
+            imagenes=new String[resultados.length()];
+            for(int i=0;i<resultados.length();i++)
+            {
+                imagenes[i]=lector.document(resultados.documentID(i)).getValues(DocumentBuilder.FIELD_NAME_IDENTIFIER)[0];
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return imagenes;
+    }
+
+}
