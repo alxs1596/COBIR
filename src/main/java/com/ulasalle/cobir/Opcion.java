5ec7ffa901e7459daec5f0bc3c9f2648114aad7e
Christian Loza <Christianlzap@gmail.com>
2017-05-14 23:07:04 -0500

    Agregado formulario

---
+++ src/main/java/com/ulasalle/cobir/Opcion.java
@@ -0,0 +1,23 @@
+package com.ulasalle.cobir;
+
+public enum Opcion {
+	CEDD,
+	FCTH,
+	AutoColorCorrelogram,
+	ColorLayout,
+	JCD,
+	ScalableColor,
+	EdgeHistogram,
+	Tamura,
+	Gabor,
+	SimpleColorHistogram,
+	OpponentHistogram,
+	JointHistogram,
+	LuminanceLayout,
+	PHOG,
+	BinaryPatternsPyramid,
+	FuzzyColorHistogram,
+	FuzzyOpponentHistogram,
+	JpegCoefficientHistogram,
+	LocalBinaryPatterns
+}
